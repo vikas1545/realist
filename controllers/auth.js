4417e0c1f1bb688e0a7870af66edbde173f0f327
vikas <vikas.theoceann@gmail.com>
2024-01-14 01:05:46 +0530

    email is getting sent..

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 import * as config from "../config.js";
+import jwt from "jsonwebtoken";
 
 export const welcome = (req, res) => {
     res.json({
@@ -8,23 +9,28 @@ export const welcome = (req, res) => {
 
 export const preRegister = async (req, res) => {
     try {
-        console.log(req.body);
-
+        //console.log(req.body);
+        const {email, password} = req.body;
+        const token = jwt.sign({email, password}, config.JWT_SECRET, {
+            expiresIn: "1h",
+        })
         const emailParams = {
-            Source: config.EMAIL_FROM,
-            Destination: {
+            Source: config.EMAIL_FROM, Destination: {
                 ToAddresses: [config.EMAIL_TO],
-            },
-            Message: {
+            }, Message: {
                 Body: {
                     Html: {
                         Charset: "UTF-8",
-                        Data: `<h1>Welcome to Realist App</h1>`,
+                        Data: `<html>
+                                 <body>
+                                  <h5>Welcome to Realist App</h5>
+                                  <p>Please click the link bellow to activate your account.</p>
+                                  <a href="${config.CLIENT_URL}/auth/account-activate/${token}">Activate my account</a>
+                                </body>
+                               </html>`,
                     },
-                },
-                Subject: {
-                    Charset: "UTF-8",
-                    Data: "Welcome to Realist",
+                }, Subject: {
+                    Charset: "UTF-8", Data: "Welcome to Realist",
                 },
             },
         };
@@ -32,9 +38,19 @@ export const preRegister = async (req, res) => {
         const data = await config.AWSSES.sendEmail(emailParams).promise();
 
         console.log('Email data:', data);
-        return res.json({ ok: true });
+        return res.json({ok: true});
     } catch (err) {
         console.error('Error:', err);
-        return res.status(500).json({ error: 'Something went wrong. Try again.' });
+        return res.status(500).json({error: 'Something went wrong. Try again.'});
     }
 };
+
+export const register = async (req,res)=> {
+    try {
+        console.log("body..",req.body);
+        return res.json({ok:"Registered..."})
+    }catch (err) {
+        console.log('err :',err);
+        return res.json({error:"Something went wrong.."})
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as auth from "../controllers/auth.js";
 
 const router = express.Router();
-router.get("/",auth.welcome)
+router.get("/",auth.welcome);
 router.post('/pre-register',auth.preRegister);
+router.post('/register',auth.register);
 export default router;
