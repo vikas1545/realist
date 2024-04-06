75997489ce38409366de55f5b09ed647524d48ac
vikas <vikas.theoceann@gmail.com>
2024-04-07 00:21:58 +0530

    login & register pages hide on loggedIn

--- src/components/nav/Main.js
+++ src/components/nav/Main.js
@@ -3,20 +3,20 @@ import {NavDropdown} from 'react-bootstrap';
 import {useAuth} from "../../context/Auth";
 
 const Main = () => {
-    const [auth,setAuth] = useAuth();
+    const [auth, setAuth] = useAuth();
     const navigate = useNavigate();
-    const logout=()=> {
-        setAuth({user:null, token:"", refreshToken:""});
+    const logout = () => {
+        setAuth({user: null, token: "", refreshToken: ""});
         localStorage.removeItem("auth");
         navigate('/login')
     }
 
-    const loggedIn= auth.user!==null && auth.token!=="" && auth.refreshToken!=="";
+    const loggedIn = auth.user !== null && auth.token !== "" && auth.refreshToken !== "";
 
-    const handlePostAdClick=()=> {
-        if(loggedIn) {
+    const handlePostAdClick = () => {
+        if (loggedIn) {
             navigate('/ad/create')
-        }else{
+        } else {
             navigate('/login')
         }
     }
@@ -26,16 +26,22 @@ const Main = () => {
         <nav className="nav d-flex justify-content-between lead">
             <NavLink className="nav-link nav-font" aria-current="page" to="/">Home</NavLink>
 
+            {
+                !loggedIn ? <>
+                    <NavLink className="nav-link nav-font" to="/login">Login</NavLink>
+                    <NavLink className="nav-link nav-font" to="/register">Register</NavLink>
+                </> : ""
 
-            <NavLink className="nav-link nav-font" to="/login">Login</NavLink>
-            <NavLink className="nav-link nav-font" to="/register">Register</NavLink>
+            }
 
-            <div className="dropdown">
-                <NavDropdown title="User" id="basic-nav-dropdown" className="nav-font">
-                    <NavLink className="dropdown-item" to="/dashboard">Dashboard</NavLink>
-                    <a className="dropdown-item" onClick={logout}>Logout</a>
-                </NavDropdown>
-            </div>
+            {
+                loggedIn ? <div className="dropdown">
+                    <NavDropdown title="User" id="basic-nav-dropdown" className="nav-font">
+                        <NavLink className="dropdown-item" to="/dashboard">Dashboard</NavLink>
+                        <a className="dropdown-item" onClick={logout}>Logout</a>
+                    </NavDropdown>
+                </div> : ''
+            }
         </nav>
 
     )
